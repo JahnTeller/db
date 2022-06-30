3bdc3a86fd32f0ea80d25682283614c61a66f33f
hnam <hnam4527@gmail.com>
2022-06-30 16:51:25 +0700

    up

--- routes/department.js
+++ routes/department.js
@@ -1,9 +1,9 @@
 const router = require("express").Router()
 const controller = require("../controllers/department")
 const { verifyAdmin, verifyToken } = require("../middlewares/middleware")
-router.post("/",verifyToken, verifyAdmin, controller.create)
-router.put("/:id",verifyAdmin,verifyToken, controller.edit)
-router.delete("/:id", verifyAdmin,verifyToken, controller.del)
+router.post("/",verifyToken,  controller.create)
+router.put("/:id",verifyToken, controller.edit)
+router.delete("/:id", verifyToken, controller.del)
 router.get("/:id", controller.get)
 router.get("/", controller.getAll)
 module.exports = router
