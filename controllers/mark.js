f4e093762629cfa01900da17368c9d86af9d5fc4
hnam <hnam4527@gmail.com>
2022-07-03 21:05:23 +0700

    up

--- controllers/mark.js
+++ controllers/mark.js
@@ -1,13 +1,13 @@
-const Mark = require("../models/mark")
+const Mark = require("../models/mark");
 const markController = {
-    getAll: async(req,res) => {
-        try {
-            const mark = await Mark.find()
-            res.status(200).json(mark)
-        } catch (error) {
-            res.status(500).json(`Erorr ${error}`)
-        }
+  getAll: async (req, res) => {
+    try {
+      const mark = await Mark.find().populate("situation");
+      res.status(200).json(mark);
+    } catch (error) {
+      res.status(500).json(`Erorr ${error}`);
     }
-}
+  },
+};
 
-module.exports = markController
+module.exports = markController;
--- models/mark.js
+++ models/mark.js
@@ -1,22 +1,23 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 
 const markSchema = new mongoose.Schema({
-    situation: {
-        type: String,
-        required: true
-    },
-    userId: {
-        type: String, 
-        required: true
-    },
-    mark: {
-        type: Number, 
-        default: 0,
-        required: true
-    },
-    times:{
-        type: Number
-    }
-})
+  situation: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Situation",
+  },
+  userId: {
+    type: String,
+    required: true,
+  },
+  mark: {
+    type: Number,
+    default: 0,
+    required: true,
+  },
+  times: {
+    type: Number,
+    default: 0,
+  },
+});
 
-module.exports = mongoose.model("Mark", markSchema)
+module.exports = mongoose.model("Mark", markSchema);
