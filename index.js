511fc8ab062f135ba4531d8b377f0118cc032b06
hnam <hnam4527@gmail.com>
2022-07-04 12:57:25 +0700

    up

--- index.js
+++ index.js
@@ -1,38 +1,47 @@
-const express = require("express")
-const cors = require("cors")
-const bodyParser = require("body-parser")
-const cookieParser = require("cookie-parser")
-const app = express()
-const morgan = require("morgan")
-const situationRouter = require("./routes/situation")
-const departmentRouter = require("./routes/department")
-const treatmentRouter = require("./routes/treatment")
-const diagnoseRouter = require("./routes/diagnose")
-const userRouter = require("./routes/user")
-const markRouter = require("./routes/mark")
-const connect = require("./config/db")
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const app = express();
+const morgan = require("morgan");
+const situationRouter = require("./routes/situation");
+const departmentRouter = require("./routes/department");
+const treatmentRouter = require("./routes/treatment");
+const diagnoseRouter = require("./routes/diagnose");
+const userRouter = require("./routes/user");
+const markRouter = require("./routes/mark");
+const connect = require("./config/db");
 
 // const { createProxyMiddleware } = require('http-proxy-middleware');
-const PORT = process.env.PORT || 8000
-app.use(bodyParser.json({ limit: "50mb", strict: false }))
+const PORT = process.env.PORT || 8000;
+app.use(bodyParser.json({ limit: "50mb", strict: false }));
 app.use(
-    bodyParser.urlencoded({
-      limit: "50mb",
-      extended: true,
-      parameterLimit: 50000,
-    })
+  bodyParser.urlencoded({
+    limit: "50mb",
+    extended: true,
+    parameterLimit: 50000,
+  })
 );
 // app.use(cors({ origin: true, credentials: true }));
 app.use(cors());
 app.use(morgan("common"));
-connect()
-app.use("/api/situation", situationRouter)
-app.use("/api/department", departmentRouter)
-app.use("/api/treatment", treatmentRouter)
-app.use("/api/diagnose", diagnoseRouter)
-app.use("/api/user", userRouter)
-app.use("/api/mark", markRouter)
-app.listen(PORT, () => {
-    console.log(`Server is running in port ${PORT}`)
-})
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.header("Access-Control-Allow-Methods", "PUT, GET, POST, DELETE, OPTIONS");
+  next();
+});
 
+connect();
+app.use("/api/situation", situationRouter);
+app.use("/api/department", departmentRouter);
+app.use("/api/treatment", treatmentRouter);
+app.use("/api/diagnose", diagnoseRouter);
+app.use("/api/user", userRouter);
+app.use("/api/mark", markRouter);
+app.listen(PORT, () => {
+  console.log(`Server is running in port ${PORT}`);
+});
