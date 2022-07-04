7f0f7ba1faba5b9ce7e91982de74d91a2b7bafeb
hnam <hnam4527@gmail.com>
2022-07-04 12:50:28 +0700

    up

--- utils/generateToken.js
+++ utils/generateToken.js
@@ -3,7 +3,7 @@ const jwt = require("jsonwebtoken");
 const generateToken = (id) => {
   return jwt.sign({ id }, process.env.JWT_SECRET, {
     expiresIn: "30d",
-    algorithm: "RS256",
+    algorithm: "HS256",
   });
 };
 
