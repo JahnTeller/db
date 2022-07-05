f696cfd2b7a3d6053a2ee85d48c86031db695a54
hnam <hnam4527@gmail.com>
2022-07-05 14:12:36 +0700

    up

--- config/db.js
+++ config/db.js
@@ -1,18 +1,23 @@
-const mongoose = require("mongoose")
-const dotenv=  require("dotenv")
-dotenv.config()
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+dotenv.config();
+const url =
+  "mongodb+srv://hnam:E95vIaIXtCZ87mUD@cluster0.hq1fa.mongodb.net/?retryWrites=true&w=majority";
+const connect = async () => {
+  try {
+    const conn = await mongoose.connect(
+      process.env.MONGO_URL || url,
+      {
+        useUnifiedTopology: true,
+        useNewUrlParser: true,
+      },
+      () => {
+        console.log("Connect successfully");
+      }
+    );
+  } catch (error) {
+    console.log(`Error ${error}`);
+  }
+};
 
-const connect = async() => {
-    try {
-        const conn = await mongoose.connect(process.env.MONGO_URL, {
-            useUnifiedTopology: true,
-            useNewUrlParser: true,
-          }, () => {
-            console.log("Connect successfully")
-          })
-    } catch (error) {
-        console.log(`Error ${error}`)
-    }
-}
-
-module.exports = connect
+module.exports = connect;
